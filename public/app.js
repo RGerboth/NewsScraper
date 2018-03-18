1cfe392c84b9049a82b943c1c55b2f068112c457
Robert Gerboth <email@example.com>
2018-03-18 15:32:46 -0600

    added byline to display

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].summary + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].byline + "<br />" + data[i].link + "<br />" + data[i].summary + "</p>");
   }
 });
 
@@ -12,7 +12,7 @@ $.getJSON("/articles", function(data) {
 $(document).on("click", "p", function() {
   // Empty the notes from the note section
   $("#notes").empty();
-  // Save the id from the p tag
+
   var thisId = $(this).attr("data-id");
 
   // Now make an ajax call for the Article
