0b02fc18da7da369a0cf145b4f8e4693446e7a12
Robert Gerboth <email@example.com>
2018-03-18 09:39:05 -0600

    additional heroku mongodb config

--- server.js
+++ server.js
@@ -26,12 +26,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsFeed";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/newsFeed", {
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
+
+// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Connect to the Mongo DB
+// mongoose.Promise = Promise;
+// mongoose.connect("mongodb://localhost/newsFeed", {
+//   useMongoClient: true
+// });
 
 // Routes
 app.get("/scrape", function(req, res) {
